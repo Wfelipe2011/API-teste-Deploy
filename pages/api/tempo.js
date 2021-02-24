e3c2cc9433bc6e43f375c16445f39bc142a2f474
Wilson Felipe <wfelipe2011@gmail.com>
2021-02-24 19:33:30 -0300

    subindo teste api

---
+++ pages/api/tempo.js
@@ -0,0 +1,9 @@
+// req é o mesmo que um pedido RESQUEST
+// res é a resposta do servidor RESPONSE
+function tempo(req, res) {
+  const dataAtual = new Date();
+
+  res.json({
+      date: dataAtual.toGMTString()
+  })
+}
