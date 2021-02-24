db59e01df6863cf3a5034bb1718ca0639970db9c
Wilson Felipe <wfelipe2011@gmail.com>
2021-02-24 19:54:58 -0300

    add stale-while-revalidade

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,6 +2,8 @@
 // res é a resposta do servidor RESPONSE
 function tempo(req, res) {
   const dataAtual = new Date();
+    // A vercel vai cachear a resposta. e a cada 10 segundo vai atualizar cache.
+    res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidade')
 
   res.json({
       date: dataAtual.toGMTString()
